0c4a77f3ba36c6ea573cbe02a30d2a3f1fcc2825
matteo ratti <matteoratti90@gmail.com>
2020-10-28 11:38:43 +0100

    main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlertService } from './services/alert.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lifebonderTest';
+export class AppComponent implements OnInit {
+  imageURL: string;
+  sharingType = 0;
+
+  constructor(private alertService: AlertService){}
+
+  ngOnInit(){}
+
+  // Image Preview
+  showPreview(event) {
+    const file = (event.target as HTMLInputElement).files[0];
+
+    // File Preview
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageURL = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form.value);
+
+    if ( form.valid){
+      this.alertService.setMessage({message: 'Post created successfully!!', type: 'alert-success'});
+    } else {
+      this.alertService.setMessage({message: 'Error! You have to fill all required fields!', type: 'alert-danger'});
+    }
+  }
+
 }
